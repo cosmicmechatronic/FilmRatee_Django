1322f3a122c42febf104d3dc3acc293bef143ddc
arysteasz <arysteasz@gmail.com>
2020-04-23 12:36:54 +0200

    Pierwszy

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -3,6 +3,22 @@ from django.http import HttpResponse
 from .models import Film, DodatkoweInfo, Ocena
 from .forms import FilmForm, DodatkoweInfoForm, OcenaForm
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, FilmSerializer
+
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class FilmView(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+
+
 
 def wszystkie_filmy(request):
     # łapie nazwe classy z pliku models
